dab6ba89cbff33b0a90732bda556d5f818c8e3af
Elliotsdreed <elliotsdreed@icloud.com>
2019-10-03 16:15:22 +0100

    Movie Class Created

---
+++ Movies.cs
@@ -0,0 +1,20 @@
+namespace Homework_Week_1_Part_3
+{
+    public class Movies
+    {
+        private string title;
+        private string runtime;
+
+        public Movies(string title, int runtime)
+        {
+            this.title=title;
+            this.runtime=runtime;
+        }
+        public int TotalDuration()
+        {
+            return (this.runtime+this/runtime);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Homework_Week_1_Part_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello passenger, How long is your flight expected to take?");
+            Console.WriteLine("60 minutes" + Environment.NewLine + "120 Minutes" + Environment.NewLine + "180 Minutes" + Environment.NewLine + "240 Minutes")
+        }
+    }
+}
